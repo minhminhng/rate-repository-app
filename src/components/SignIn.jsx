48c632338405594ce65cd88d26314634bba35c9f
Minh Nguyen <minhnguyen93@outlook.com>
2024-01-31 19:32:28 +0200

    Test sign-in form (exercise 10.18)

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -52,6 +52,18 @@ const SignInForm = ({ onSubmit }) => {
   )  
 }
 
+export const SignInContainer = ({ onSubmit }) => {
+  
+  return (
+    <Formik 
+      initialValues={initialValues} 
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  ); 
+}
+
 const SignIn = () => {
   const [signIn] = useSignIn();
   const navigate = useNavigate();
@@ -69,12 +81,7 @@ const SignIn = () => {
   };
 
   return (
-    <Formik 
-      initialValues={initialValues} 
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}>
-      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-    </Formik>
+    <SignInContainer onSubmit={onSubmit} />
   ); 
 };
 
